0c6356cfc0cba98ba8610eb832c35a01f4e208f6
TADHG CODYRE(19-09-2019 13-29 <danu7_ct@danu7>
2020-01-16 14:47:51 +0000

    Added database schemas

---
+++ models/post.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+require('./util');
+
+var postSchema = new Schema({
+    user_name: {type: String},
+	//file_name: {type: String},
+    comment: {type: String},
+    date_created : {type: Date, default: new Date()},
+    up_votes: {type: Number, default : 0},
+    down_votes: {type: Number, default : 0}
+});
+
+module.exports = mongoose.model('Post', postSchema);
---
+++ models/users.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+require('./util');
+
+var usersSchema = new Schema({
+	user_name: {type: String},
+	password: String,
+	access_token: String
+});
+
+usersSchema.methods.generateHash = function(password){
+	return bcrypt.hashsync(password, bcrypt.genSaltSync(8));
+};
+
+usersSchema.methods.generatehash = function(password){
+	return bcrypt.compareSync(password, this.password);
+};

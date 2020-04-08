499709e3d56fb459a5357ea8a75e9449e844424f
MairtinOFlatharta <46865705+MairtinOFlatharta@users.noreply.github.com>
2020-04-08 16:00:24 +0100

    Update util.js

--- models/util.js
+++ models/util.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://mongodb2140be:as1sos@danu7.it.nuigalway.ie:8717/mongodb2140');
+var connection = mongoose.connect(process.env.connection);
 
 exports.connection = connection;
